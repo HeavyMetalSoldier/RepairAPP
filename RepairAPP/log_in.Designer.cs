16f4dd1314a844c100b9132403e2da63de0c093e
HeavyMetalSoldier <art.nab47@gmail.com>
2022-11-12 21:09:06 +0500

    Добавьте файлы проекта.

---
+++ RepairAPP/log_in.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace RepairAPP
+{
+    partial class log_in
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_login.Location = new System.Drawing.Point(130, 100);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(291, 39);
+            this.textBox_login.TabIndex = 0;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_password.Location = new System.Drawing.Point(130, 211);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.PasswordChar = '*';
+            this.textBox_password.Size = new System.Drawing.Size(291, 39);
+            this.textBox_password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(227, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(221, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(213, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ВОЙТИ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // log_in
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(565, 430);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_login);
+            this.Name = "log_in";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ RepairAPP/log_in.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using RepairAPP;
+
+namespace RepairAPP
+{
+    public partial class log_in : Form
+    {
+        DataBase dataBase = new DataBase();
+        public log_in()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void log_in_Load(object sender, EventArgs e)
+        {
+            textBox_login.MaxLength = 50;
+            textBox_password.MaxLength = 50;
+            textBox_password.PasswordChar = '*';
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            var UserLogin = textBox_login.Text;
+            var UserPassword = textBox_password.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable dataTable = new DataTable();
+
+            string loginQuery = $"select UserID, UserLogin, UserPassword from Register where UserLogin = '{UserLogin}' and UserPassword = '{UserPassword}'";
+            SqlCommand command = new SqlCommand(loginQuery, dataBase.getConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(dataTable);
+
+            if (dataTable.Rows.Count == 1)
+            {
+                MessageBox.Show("Вы успешно вошли");
+                FormMain form1 = new FormMain();
+                this.Hide();
+                form1.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Введен неправильный логин или пароль");
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            sign_up formsign = new sign_up();
+            formsign.Show();
+            this.Hide();
+        }
+    }
+}
